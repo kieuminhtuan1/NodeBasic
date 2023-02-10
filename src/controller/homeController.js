095e2cda8786bdd9cf92d13eec2fe2901be22106
kieuminhtuan1 <kieuminhtuan2003k@gmail.com>
2023-02-10 20:54:16 +0700

    commit7

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -20,7 +20,7 @@ let getHomepage = (req, res) => {
             });
 
 
-            return res.render('index.ejs', { dataUser: JSON.stringify(data) })
+            return res.render('index.ejs', { dataUser: data, test: "abc string test" })
         });
 
 
