9ecf2435873dc63daf49cb961b63da26acbe08d0
kieuminhtuan1 <kieuminhtuan2003k@gmail.com>
2023-02-16 08:47:30 +0700

    commit 9

--- src/route/wed.js
+++ src/route/wed.js
@@ -3,7 +3,8 @@ import homeController from '../controller/homeController';
 let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
-    router.get('/detail/user/:id', homeController.getDetailPage)
+    router.get('/detail/user/:id', homeController.getDetailPage);
+    router.post('/create-new-user', homeController.createNewUser)
     router.get('/about', (req, res) => {
         res.send(`My name is Tuan`)
     })
